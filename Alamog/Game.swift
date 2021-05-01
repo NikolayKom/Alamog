d248b56ebd130ee952b7f017da865c77aa3ead52
Nikolay <k70456523@gmail.com>
2021-05-01 12:27:18 +0700

    Added: -Extension for Collection (Who check only one card isFaceUp or not) -Closures for get indexOfOneAndOnlyFaceUpCard who filtered FaceUp card in cards

--- Alamog/Game.swift
+++ Alamog/Game.swift
@@ -9,25 +9,31 @@ import Foundation
 class Game {
     var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     func chooseCard(at index: Int) {
         if !cards[index].isMathed {
             if let matchingIndex = indexOfOneAndOnlyFaceUpCard,
                matchingIndex != index {
-                if cards[matchingIndex].identifier ==
-                    cards[index].identifier {
+                if cards[matchingIndex] ==
+                    cards[index] {
                     cards[matchingIndex].isMathed = true
                     cards[index].isMathed = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
-                for flipDown in cards.indices {
-                    cards[flipDown].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
-                    indexOfOneAndOnlyFaceUpCard = index
+                indexOfOneAndOnlyFaceUpCard = index
                 }
             }
     }
@@ -39,4 +45,12 @@ class Game {
         }
         cards.shuffle()
     }
+}
+
+extension Collection {
+    
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
+    
 }
