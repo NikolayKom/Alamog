2a8ad34dde0e29ecfa6eaca071c4edb76d28a223
Nikolay <k70456523@gmail.com>
2021-05-02 19:55:20 +0700

    Finally version added: -Best result is writted now to a separate variable and displayed in game -Alert about winning with score

--- Alamog/ViewController.swift
+++ Alamog/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //Mark: Create game cards
     lazy var game = Game(numberOfPairsOfCards:
             (buttonCollection.count + 1)/2)
-    
+    //Mark: Set on change touches
     var touches = 0 {
         didSet{
             let attributes: [NSAttributedString.Key: Any] = [
@@ -23,10 +23,17 @@ class ViewController: UIViewController {
         }
     }
     
+    override func viewDidLoad() {
+        //undo load game loading userdefaults bestscore
+        let bestScoreData = defaults.integer(forKey: "BestScore")
+        bestScoreChecker(bestScoreData: bestScoreData)
+        
+        
+    }
+    let defaults = UserDefaults.standard
     var emojiCollection = ["🌛","🌜","🌚","🌕","🌖","🌗","🌘","🌑","🌒","🌓","🌔","🍄","⛅️","🌨","🌩"]
-    
     var emojiDictionary = [Card:String]()
-    
+    //Mark: randomaize emoji
     func emojiIdentifier(for card: Card) -> String {
         if emojiDictionary[card] == nil {
             let randomIndex =
@@ -37,6 +44,16 @@ class ViewController: UIViewController {
         return emojiDictionary[card] ?? "?"
     }
     
+    func bestScoreChecker(bestScoreData: Int) {
+        if bestScoreData == 0 {
+            defaults.set(9999, forKey: "BestScore")
+            bestScoreLabel.text = "Best Score: 0"
+        } else {
+            bestScoreLabel.text = "Best Score: \(bestScoreData)"
+        }
+    }
+    
+    //Mark: Update view after click the card
     func updateViewFromModel() {
         for index in buttonCollection.indices {
             
@@ -52,7 +69,7 @@ class ViewController: UIViewController {
         }
     }
 
-    
+    //Mark: Check status game, if win reload game, compare BestScore, show notification
     func maybeWin() {
         var indexOfCards = 0
         
@@ -65,22 +82,42 @@ class ViewController: UIViewController {
         
         
         if indexOfCards == buttonCollection.count {
-            print(indexOfCards)
-            print(buttonCollection.count)
+            var bestScoreData = defaults.integer(forKey: "BestScore")
+            indexOfCards = 0
             touchLabel.text = "You are Win!"
-            
-            let alert = UIAlertController(title: "You are Win!", message: "Your score \(touches)", preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
 
+            
+            let alert = UIAlertController(title: "You are Win!", message: "Your score: \(touches)", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Next game", style: .cancel, handler: { [self]action in touches = 0}))
             self.present(alert, animated: true)
+            
+            for index in buttonCollection.indices {
+                game.cards[index].isMathed = false
+                game.cards[index].isFaceUp = false
+                let button = buttonCollection[index]
+                button.setTitle("", for: .normal)
+                button.backgroundColor =  #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+                }
+          
+            if touches < bestScoreData {
+                defaults.set(touches, forKey: "BestScore")
+                bestScoreData = defaults.integer(forKey: "BestScore")
+                bestScoreLabel.text = "Best Score: \(bestScoreData)"
+                print((defaults.integer(forKey: "BestScore")))
+            }
+                
+                
+                
+            
         }
-    
     }
     
-
+    
+    
     @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet weak var touchLabel: UILabel!
+    @IBOutlet weak var bestScoreLabel: UILabel!
     
     @IBAction func buttonAction(_ sender: UIButton) {
         touches += 1
